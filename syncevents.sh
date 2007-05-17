47882d6b94f431d15e0e48fd4d57ac5b5f392ecf
Hermann Schwarting <hfs@gmx.de>
2007-05-17 22:32:55 +0000

    * Txt2blink kostet CPU. Es werden nur die Animationen neu generiert, die neu   sind oder sich geändert haben. * Neue Abhängigkeit: xmlstarlet (http://xmlstar.sourceforge.net/) o.ä.

--- syncevents.sh
+++ syncevents.sh
@@ -5,6 +5,7 @@
 # Either put these in the $PATH or insert the full path here:
 GETEVENTS=getevents.py
 TXT2BLINK=txt2blink.py
+XMLSTARLET=xmlstarlet
 
 if [ "$2" == "" ]; then
 	echo "Usage: $0 source-URL target-directory"
@@ -14,11 +15,21 @@ fi
 cd "$2"
 
 # Fetch current event listing and make movies.
-# Intention of the temporary files: At all points in time there should be some
-# valid .bml files
+# Intention of the temporary files: At all points in time there should be only
+# valid .bml files. Check if the contents have changed, because generating
+# the movie costs CPU.
 $GETEVENTS $1 | while read date; do
 	read text
-	$TXT2BLINK -2 "$text" > $date.bml.tmp
+	if [ -e $date.bml ]; then
+		lasttext=$($XMLSTARLET sel -t -v /blm/header/description $date.bml)
+		if [ "$lasttext" == "$text" ]; then
+			cp $date.bml $date.bml.tmp
+		else
+			$TXT2BLINK -2 "$text" > $date.bml.tmp
+		fi
+	else
+		$TXT2BLINK -2 "$text" > $date.bml.tmp
+	fi
 done
 
 shopt -s nullglob
